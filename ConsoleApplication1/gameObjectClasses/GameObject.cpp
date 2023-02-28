6aad6cdec4498a916b2d74701b85f5ab40d79987
rstrojan <32496785+rstrojan@users.noreply.github.com>
2023-02-28 09:41:04 -0800

    Added notes to the methods.

--- ConsoleApplication1/gameObjectClasses/GameObject.cpp
+++ ConsoleApplication1/gameObjectClasses/GameObject.cpp
@@ -8,24 +8,31 @@ GameObject::GameObject(std::string name)
 	generateSlotDetailMap();
 }
 
+//Returns a string of key details of the object
+// for use as a choice in a Prompt.
 std::string GameObject::getChoiceDetailString()
 {
 	generateChoiceDetailString();
 	return choiceDetailString;
 }
-
+//Creates or updates a string of key details of the 
+// object for use as a choice in a Prompt.
 void GameObject::generateChoiceDetailString()
 {
 	choiceDetailString = name;
 	return;
 }
 
+//Returns a reference of map<string, string> that has key
+// details about the object for use in a PrntScrn slot.
 std::map<std::string, std::string>& GameObject::getSlotDetailMap()
 {
 	generateSlotDetailMap();
 	return slotDetailMap;
 }
 
+//Creates or updates a reference of a map<string, string> that has
+// key details about the object for use in a PrntScrn slot.
 void GameObject::generateSlotDetailMap()
 {
 	slotDetailMap.clear();
